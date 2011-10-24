51498176f6f3ecf1cafb5d55f8b0b4404b3d1b93
roboyeti <roboyeti@gmail.com>
2011-10-24 01:28:12 -0700

    * (VERSION 0.3.5) Added dedication fields track_type (type is already a DB column), category, caption to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,11 +51,10 @@ end
 #  dedication
 #
 #
-# row => title,description,type,category,captioned?,image,audio,video,preview,thumbnail,level,star cost,aff_text,aff_link
-#def self.default(stub, title,description,preview,thumbnail, level,star_cost, image,audio,video,aff_text,aff_link)
-
+# row => title,description,track_type,category,captioned?,image,audio,video,preview,thumbnail,level,star cost,aff_text,aff_link
+#def self.default(stub, title,description,track_type,category,captioned?,preview,thumbnail, level,star_cost, image,audio,video,aff_text,aff_link)
 CSV.read("#{base}/seeddata/dedications.csv").select{|x| x[0].downcase != "title"}.each do |row|
-  Dedication.default(row[0].downcase,row[0],row[1],"#{base}/seeddata/assets/#{row[8]}","#{base}/seeddata/assets/#{row[9]}",row[10],row[11], "#{base}/seeddata/assets/#{row[5]}","#{base}/seeddata/assets/#{row[6]}","#{base}/seeddata/assets/#{row[7]}",row[12],row[13])
+  Dedication.default(row[0].downcase,row[0],row[1],row[2],row[3],row[4],"#{base}/seeddata/assets/#{row[8]}","#{base}/seeddata/assets/#{row[9]}",row[10],row[11],"#{base}/seeddata/assets/#{row[5]}","#{base}/seeddata/assets/#{row[6]}","#{base}/seeddata/assets/#{row[7]}",row[12],row[13])
 end
 
 # old
