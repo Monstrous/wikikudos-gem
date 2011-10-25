fae527ba14ec51da506c286f9573bf7843d1a9ea
Corprew Reed <github@corprew.org>
2011-10-25 08:53:10 -0700

    adds the artist policy IOC static class

---
+++ lib/candy-gem/artist_policy.rb
@@ -0,0 +1,55 @@
+class ArtistPolicy
+  class BadgeHook
+    def self.added_a_new_user(profile,new_user,new_val)
+      
+      badges = {7=> "Schmoozinator", 13 => "Lucky 13"}
+
+      badges.each do | k, v |
+        if new_val >= k && new_val - count < k
+          b = profile.add_badge_by_name(v)
+          profile.acquired_badges << b unless b.nil?
+        end
+      end
+      
+    end
+    
+    def self.update_player_badge_progress(p,recipient_count)
+      badges = {1 => "Welcome to the Horde", 27 => "Accomplished Monsterizer", 103 => "Deliverator"}
+
+      badges.each do | k, v |
+        if new_val >= k && new_val - recipient_count < k
+          b = profile.add_badge_by_name(v)
+          profile.acquired_badges << b unless b.nil?
+        end
+      end
+      
+    end
+    
+    def self.add_badge(b)
+      #
+      #  This handler not needed for Monstergrams.
+      #
+    end
+    
+    def self.sent_a_message
+      #
+      #   This handler not wired in yet
+      #
+    end
+    
+    def self.received_a_message
+      #
+      #   This handler not wired in yet.
+      #
+    end
+  end
+  
+  class DedicationHook
+  end
+  
+  class GearHook
+  end
+  
+  class MessageHook
+  end
+end
